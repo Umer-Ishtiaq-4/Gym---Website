97c84bb741a6e8615217c3994a5653c09ac1bbdf
Umer-Ishtiaq-4 <umerishtiaq4@gmail.com>
2023-10-26 00:57:40 +0500

    DB Updated

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo "Build Started"
 python3 -m pip install -r requirements.txt
-python3 -m pip install psycopg2
+python3 -m pip install psycopg2-binary
+
 python3 manage.py collectstatic --noinput --clear
 echo "Build Ended"
