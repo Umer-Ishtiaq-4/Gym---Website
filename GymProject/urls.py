ba91e0d7505f49d49c81b8c3d05e35e8dd2281e1
Umer-Ishtiaq-4 <umerishtiaq4@gmail.com>
2023-10-27 05:38:16 +0500

    Djoser Code Updated

--- GymProject/urls.py
+++ GymProject/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('cred/', include('userprofile.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    # path('auth/', include('djoser.urls')),
+    # path('auth/', include('djoser.urls.jwt')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
